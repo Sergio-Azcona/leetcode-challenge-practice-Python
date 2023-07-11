cab6373a015b2ec5de49679b942d36c705904b5b
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-07-10 22:12:56 -0400

    completed: range method, append, sum

---
+++ complete/multiples_of_3_or_5_6kyu.py
@@ -0,0 +1,39 @@
+# https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
+# 6 kyu - Multiples of 3 or 5
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. 
+# Additionally, if the number is negative, return 0 (for languages that do have them).
+# Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(number):
+  collection = [] #collect all relevant #s
+  
+#exclude numbers less than 4
+  if number >= 4:
+  # iterate through numbers, starting from 1 to number
+    for i in range(1, number):
+      #each number that is divisible by 3 or 5
+      if i % 3 == 0 or i % 5 == 0:
+      # added to collection
+        collection.append(i)
+        
+  #return the sum of all digits in collection
+  return sum(collection)  
+    # pass
+  
+
+
+
+s1 = solution(4) # 3
+s2 = solution(6) #8 
+s3 = solution(16) #60
+s4 = solution(3) # 0
+s5 = solution(-15) #0
+s6 = solution(0) #0
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
+print(s6)
